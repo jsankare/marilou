f93856d2a8da93a20d6584ef06c495eb9029c651
Brubru <jordan.sankare@gmail.com>
2023-02-09 11:30:50 +0100

    ..

--- js/prestations.js
+++ js/prestations.js
@@ -3,7 +3,7 @@ function presentationTitle() {
     const presentation = document.getElementById('presentation__container')
     const title = document.createElement('h1')
     title.id = `presentation__title`
-    title.innerText = `Nos prestations`
+    title.innerText = `Mes prestations`
     presentation.appendChild(title)
 }
 presentationTitle()
