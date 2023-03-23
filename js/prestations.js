3a6a93016dcec06c4bc5bef99a9269a6207b7b8d
jordan sankare <jordan.sankare@gmail.com>
2023-03-23 22:04:43 +0100

    minor tweaks to content and removed forfaits

--- js/prestations.js
+++ js/prestations.js
@@ -26,7 +26,7 @@ function prices() {
         thirty: '9€',
         fortyFive: '12€',
         hour: `15€`,
-        home: `10€ pour 1 animal<br>12€ pour 2 animaux<br>15€ à partir de 3 animaux`,
+        home: `15€ pour 1 animal<br>20€ pour 2 animaux<br>25€ à partir de 3 animaux`,
         smallSize: `Pour les chats, lapins, rongeurs ou autre animal domestique de petite taille, le prix est de 5€`
     }
 
@@ -35,7 +35,7 @@ function prices() {
         thirty: '',
         fortyFive: '',
         hour: ``,
-        home: `* À partir de 3 visites par jour, le prix augmente de 5€`,
+        home: `*Prix pour 1 à 2 visite(s). À partir de 3 visites par jour, le prix augmente de 5€`,
         smallSize: ``
     } // refaire bien 
 
@@ -59,31 +59,31 @@ function prices() {
 }
 prices()
 
-function packages() {
-    const cards = document.getElementById('cards_2')
+// function packages() {
+//     const cards = document.getElementById('cards_2')
 
-    const price__cards = {
-        thirty: 'Visite de jour/soir pendant -6 mois',
-        fortyFive: 'Visite de jour/soir pendant + 6 mois',
-    };
+//     const price__cards = {
+//         thirty: 'Visite de jour/soir pendant -6 mois',
+//         fortyFive: 'Visite de jour/soir pendant + 6 mois',
+//     };
 
-    const prices = {
-        thirty: '11€ / jour',
-        fortyFive: '9€ / jour',
-    }
+//     const prices = {
+//         thirty: '20€ / jour',
+//         fortyFive: '17€ / jour',
+//     }
 
-    for (const [link, label] of Object.entries(price__cards)) {
-        const cardContainer = document.createElement('div')
-        const cardText = document.createElement('p')
-        const cardPrice = document.createElement('span')
+//     for (const [link, label] of Object.entries(price__cards)) {
+//         const cardContainer = document.createElement('div')
+//         const cardText = document.createElement('p')
+//         const cardPrice = document.createElement('span')
 
-        cardPrice.innerHTML = `${prices[link]}`
-        cardContainer.className = `card flex center`
-        cardText.innerText = `${label} :`
-        cardContainer.appendChild(cardText)
-        cardContainer.appendChild(cardPrice)
-        cards.appendChild(cardContainer)
-    }
-}
-packages()
+//         cardPrice.innerHTML = `${prices[link]}`
+//         cardContainer.className = `card flex center`
+//         cardText.innerText = `${label} :`
+//         cardContainer.appendChild(cardText)
+//         cardContainer.appendChild(cardPrice)
+//         cards.appendChild(cardContainer)
+//     }
+// }
+// packages()
 
