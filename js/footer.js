c74e83413077c5e34236162747b96473a56907ae
jordan sankare <jordan.sankare@gmail.com>
2023-03-29 21:44:13 +0200

    phone in footer now works

--- js/footer.js
+++ js/footer.js
@@ -36,7 +36,7 @@ function footer() {
 
     const phone = document.createElement('a')
     phone.href = `tel:0674802188`
-    phone.innerHTML = `06.74.80.21.88` // does not work
+    phone.innerHTML = `06.74.80.21.88`
     infos.appendChild(phone)
 }
 footer()
