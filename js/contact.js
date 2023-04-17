491b70728dc59d101f5779ec356bbafdfb973cdf
jordan sankare <jordan.sankare@gmail.com>
2023-04-17 10:04:25 +0200

    finished getting all infos from contact form into an object stored in localstorage

--- js/contact.js
+++ js/contact.js
@@ -5,21 +5,24 @@ document.querySelector(".inputBtn").addEventListener("click", function(event) {
     const lastName = document.querySelector("input[name='lastName']").value
     const email = document.querySelector("input[name='email']").value
     const phone = document.querySelector("input[name='phone']").value
-    // enter others inputs here
+    const address = document.querySelector("input[name='address']").value
+    const city = document.querySelector("input[name='city']").value
+    const date = document.querySelector("input[name='date']").value
+    const time = document.querySelector("input[name='hour']").value
     const number = document.querySelector("input[name='number']").value
+    const race = document.querySelector("input[name='pets']").value
     const message = document.querySelector("textarea").value
   
-    const firstNameRegex = /^[a-zA-Z]+$/
-    const lastNameRegex = /^[a-zA-Z]+$/
+    const textRegex = /^[a-zA-Z]+$/
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     const phoneRegex = /^\d{10}$/
   
-    if (firstName === "" || !firstName.match(firstNameRegex)) {
+    if (firstName === "" || !firstName.match(textRegex)) {
       alert("Veuillez entrer un prénom valide")
       return
     }
   
-    if (lastName === "" || !lastName.match(lastNameRegex)) {
+    if (lastName === "" || !lastName.match(textRegex)) {
       alert("Veuillez entrer un nom valide")
       return
     }
@@ -31,6 +34,36 @@ document.querySelector(".inputBtn").addEventListener("click", function(event) {
   
     if (phone === "" || !phone.match(phoneRegex)) {
       alert("Veuillez entrer un numéro de téléphone valide")
+      return
+    }
+
+    if (address === "") {
+      alert("Veuillez entrer votre adresse")
+      return
+    }
+
+    if (city === "") {
+      alert("Veuillez entrer votre ville")
+      return
+    }
+
+    if (date === "") {
+      alert("Veuillez entrer une date de garde")
+      return
+    }
+
+    if (time === "") {
+      alert("Veuillez entrer un horaire de garde")
+      return
+    }
+
+    if (number === "") {
+      alert("Veuillez entrer un nombre d'animaux à garder")
+      return
+    }
+
+    if (race === "") {
+      alert("Veuillez entrer un nombre d'animaux à garder")
       return
     }
   
@@ -46,6 +79,12 @@ document.querySelector(".inputBtn").addEventListener("click", function(event) {
       Nom: lastName,
       Email: email,
       Téléphone: phone,
+      Adresse : address,
+      Ville : city,
+      'Date de garde' : date,
+      Horaire : time,
+      Nombre : number,
+      Type : race,
       Message: message
     });
   
@@ -57,5 +96,11 @@ document.querySelector(".inputBtn").addEventListener("click", function(event) {
     document.querySelector("input[name='lastName']").value = ""
     document.querySelector("input[name='email']").value = ""
     document.querySelector("input[name='phone']").value = ""
+    document.querySelector("input[name='address']").value = ""
+    document.querySelector("input[name='city']").value = ""
+    document.querySelector("input[name='date']").value = ""
+    document.querySelector("input[name='hour']").value = ""
+    document.querySelector("input[name='number']").value = ""
+    document.querySelector("input[name='pets']").value = ""
     document.querySelector("textarea").value = ""
 })
